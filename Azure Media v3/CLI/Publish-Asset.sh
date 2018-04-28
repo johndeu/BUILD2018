c9905f4a7d6fd7943f74953f06d4745e9ace3876
John Deutscher <johndeu@microsoft.com>
2018-04-28 12:53:07 -0700

    fixed publish

--- Azure Media v3/CLI/Publish-Asset.sh
+++ Azure Media v3/CLI/Publish-Asset.sh
@@ -5,8 +5,14 @@ resourceGroup=build2018
 amsAccountName=build18
 assetName="myAsset-uniqueID"
 locatorName="myStreamingLocator"
-streamingPolicyName="PredefinedClearStreamingOnly"
+streamingPolicyName="Predefined_DownloadAndClearStreaming"
 #contentPolicyName=""
+
+# Delete the locator if it already exists
+az ams streaming locator delete \
+    -a $amsAccountName \
+    -g $resourceGroup \
+    -n $locatorName \
 
 # Create a new Streaming Locator. Modify the assetName variable to point to the Asset you want to publish
 # This uses the predefined Clear Streaming Only policy, which allows for unencypted deliver over HLS, Smooth and DASH protocols. 
@@ -16,11 +22,12 @@ az ams streaming locator create \
     --asset-name $assetName \
     -n $locatorName \
     --streaming-policy-name $streamingPolicyName \
-    #--end-time 2100-10-10T00:00:00Z \
+    --end-time 2100-10-10T00:00:00Z \
     #--start-time 2018-04-28T00:00:00Z \
     #--content-policy-name $contentPolicyName \
 
 # List the Streming URLs relative paths for the new locator.  You must append your Streaming Endpoint "hostname" path to these to resolve the full URL. 
+# Note that the asset must have an .ismc and be encoded for Adaptive streaming in order to get Streaming URLs back. You can get download paths for any content type.
 az ams streaming locator get-paths \
     -a $amsAccountName \
     -g $resourceGroup \
